8e42e6c9af724731c5861a6fdcbabbd4d7cf4ba6
Chad <chad.cuomo@yahoo.com>
2019-11-14 21:06:04 -0500

    updated app for deployment on github pages

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 import '../containers/App.css';
 
 
@@ -15,7 +16,7 @@ class App extends Component {
     }
 
     componentDidMount() {
-        fetch('http://jsonplaceholder.typicode.com/users').then(response=> response.json())
+        fetch('https://jsonplaceholder.typicode.com/users').then(response=> response.json())
         .then(users => this.setState({ robots: users }));
     }
 
@@ -35,7 +36,9 @@ class App extends Component {
                 <h1 className='f1'>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <Scroll>
+                    <ErrorBoundry>
                     <CardList robots={filteredRobots}/>
+                    </ErrorBoundry>
                 </Scroll>
             </div>
             ); 
