fe40a52b854194d26292c96ae95640e86bfca91f
Chad <chad.cuomo@yahoo.com>
2019-11-14 20:51:06 -0500

    Updates

---
+++ precache-manifest.d6b02a17c38b29bd398223e3825e6342.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2b4e0be350d9144deaff64d859d7aa8",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "04130c833c2154f9d603",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "c9392e39d3478b7f4bbe",
+    "url": "/robofriends/static/css/main.37984727.chunk.css"
+  },
+  {
+    "revision": "04130c833c2154f9d603",
+    "url": "/robofriends/static/js/2.ada142a8.chunk.js"
+  },
+  {
+    "revision": "c9392e39d3478b7f4bbe",
+    "url": "/robofriends/static/js/main.0a405b02.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
